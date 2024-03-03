ac1e25c24062499fbfb42c6453d7d79b045fe612
Suthar Jay <sutharjay3635@gmail.com>
2024-03-03 16:22:05 +0530

    share link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -20,7 +20,7 @@ const NavBar = () => {
     <>
       <div className="w-full flex items-center justify-center">
         <div className="lg:min-w-[65%] md:min-w-[95%] min-w-[95%] h-auto flex items-center justify-between  px-8 py-4 mt-4 rounded-md backdrop-blur-md bg-rose-700/80 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-          <Link to={userSlug && `/home/${userSlug}`}>
+          <Link to={`${userSlug ? `/home/${userSlug}` : '/login'}`}>
             <div>
               <h1 className="text-3xl font-bold text-white">Photos</h1>
             </div>
